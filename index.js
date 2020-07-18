4a0967c31c98942e5d6459ae96cb24839798dedd
Sirwan <sir1afifi@gmail.com>
2020-07-18 14:00:00 +0100

    Add pie chart

--- index.js
+++ index.js
@@ -1,21 +1,23 @@
+const results = [
+  { name: "Satisfied", count: 1043, color: "lightblue" },
+  { name: "Neutral", count: 563, color: "lightgreen" },
+  { name: "Unsatisfied", count: 510, color: "pink" },
+  { name: "No comment", count: 175, color: "silver" },
+];
+
 let cx = document.querySelector("canvas").getContext("2d");
+let total = results.reduce((sum, { count }) => sum + count, 0);
+let currentAngle = -0.5 * Math.PI;
+let centerX = 300,
+  centerY = 150;
 
-cx.strokeStyle = "dodgerblue";
-cx.strokeRect(0, 0, 500, 50);
-cx.lineWidth = 1;
-cx.strokeRect(135, 0, 50, 50);
-
-cx.fillStyle = "tomato";
-cx.fillRect(0, 0, 100, 50);
-
-drawLine(10, 1000, 10);
-drawLine(10, 1000, 1000);
-
-function drawLine(from, to, column) {
+// Add code to draw the slice labels in this loop.
+for (let result of results) {
+  let sliceAngle = (result.count / total) * 2 * Math.PI;
   cx.beginPath();
-  for (let y = from; y < to; y += 10) {
-    cx.moveTo(column, y);
-    cx.lineTo(90, y);
-  }
-  cx.stroke();
+  cx.arc(centerX, centerY, 100, currentAngle, currentAngle + sliceAngle);
+  currentAngle += sliceAngle;
+  cx.lineTo(centerX, centerY);
+  cx.fillStyle = result.color;
+  cx.fill();
 }
