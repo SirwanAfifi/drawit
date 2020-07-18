f14e0e60828c5f5690b1389197d74b13678ecf06
Sirwan <sir1afifi@gmail.com>
2020-07-18 14:00:00 +0100

    Draw line

--- index.js
+++ index.js
@@ -7,3 +7,15 @@ cx.strokeRect(135, 0, 50, 50);
 
 cx.fillStyle = "tomato";
 cx.fillRect(0, 0, 100, 50);
+
+drawLine(10, 1000, 10);
+drawLine(10, 1000, 1000);
+
+function drawLine(from, to, column) {
+  cx.beginPath();
+  for (let y = from; y < to; y += 10) {
+    cx.moveTo(column, y);
+    cx.lineTo(90, y);
+  }
+  cx.stroke();
+}
