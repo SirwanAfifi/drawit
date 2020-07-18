679d5a1c73f8846de9685e08fd67bb9d3b68f7e2
Sirwan <sir1afifi@gmail.com>
2020-07-18 14:00:00 +0100

    First attempts

---
+++ index.js
@@ -0,0 +1,9 @@
+let cx = document.querySelector("canvas").getContext("2d");
+
+cx.strokeStyle = "dodgerblue";
+cx.strokeRect(0, 0, 500, 50);
+cx.lineWidth = 1;
+cx.strokeRect(135, 0, 50, 50);
+
+cx.fillStyle = "tomato";
+cx.fillRect(0, 0, 100, 50);
